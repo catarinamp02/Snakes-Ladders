cef75947963cb13bd344c47b1d3fd3c840758e35
CATARINA MACEDO PIMENTA <catarinamp02@gmail.com>
2024-04-27 22:19:44 +0100

    Base com textura e bibliotecas

--- JavaScript/appThree.js
+++ JavaScript/appThree.js
@@ -1,55 +1,63 @@
-const renderer = new THREE.WebGLRenderer();
-//janela
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xaaaaaa);
+import * as THREE from 'three';
+import {OrbitControls} from 'OrbitControls';
 
-document.body.appendChild(renderer.domElement);
-
-const cena = new THREE.Scene();
-
-//Camaras 
-//Ortográfica
-const CamaraOrtografica = new THREE.OrthographicCamera(
-    -1,     //left
-    1,      //right 
-    1,      //top
-    -1,     //bottom
-    -10,    //near plane    
-    10      //far plane
-);
-
-//Perspetiva 
-const CamaraPerspetiva = new THREE.PerspectiveCamera(
-    75,                                                     //fov
-    window.innerWidth/window.innerHeight,                   //aspect ratio
-     0.1,                                                   //near plane
-     1000                                                 //far plane 
-
-);
-
-const AxesHelper = new THREE.AxesHelper(5);
-cena.add(AxesHelper);
-
-var texturaMesa = new THREE.TextureLoader().load('./Imagens/madeira.jpg');
-
-var gemoetriaCubo = new THREE.BoxGeometry(20,1,20);
-var materialCubo = new THREE.MeshBasicMaterial({map:texturaMesa});
-var mesa = new THREE.Mesh(gemoetriaCubo,materialCubo);
-mesa.position.set(0,-1,0);
-cena.add(mesa);
-
-CamaraPerspetiva.position.set(0,10,28); 
-//controls.update();
+var scene, camera, renderer, controls;
+ 
+function init() {
+  
+ 
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+ 
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0xaaaaaa);
+  document.body.appendChild(renderer.domElement);
 
 
-//const controls = new OrbitControls(CamaraPerspetiva, renderer.domElement);
+  var texturaMesa = new THREE.TextureLoader().load('./Imagens/madeira.jpg');
 
-//Para ajustar o tamanho da janela 
-CamaraPerspetiva.aspect = window.innerWidth / window.innerHeight;
-CamaraPerspetiva.updateProjectionMatrix();
+ 
+  var gemoetriaCubo = new THREE.BoxGeometry(20,1,20);
+  var materialCubo = new THREE.MeshBasicMaterial({map:texturaMesa});
+  var base = new THREE.Mesh(gemoetriaCubo,materialCubo);
+  base.position.set(0,-1,0);
+  scene.add(base);
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.position.set(0,10,28); 
 
-renderer.render(cena, CamaraPerspetiva);
-
-
+  const AxesHelper = new THREE.AxesHelper(5);
+  scene.add(AxesHelper);
+ 
+  controls = new OrbitControls(camera, renderer.domElement);
+ 
+  controls.target.set(4.5, 0, 4.5);
+ 
+  controls.enablePan = false;
+  controls.maxPolarAngle = Math.PI / 2;
+ 
+  controls.enableDamping = true;
+ 
+  window.requestAnimationFrame(animate);
+}
+ 
+function animate() {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(animate);
+}
+ 
+//ajustar a janela
+function onWindowResize() {
+ 
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+ 
+  renderer.setSize( window.innerWidth, window.innerHeight );
+ 
+}
+ 
+ 
+window.addEventListener('resize', onWindowResize);
+ 
+window.onload = init;
