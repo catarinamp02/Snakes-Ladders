406b14fc401532c508ae560f2b3f931869e85bca
GoncaloSilvaUTAD <al60743@alunos.utad.pt>
2024-05-06 17:57:22 +0100

    adicionei boneco neve

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ function init() {
   base.position.set(0,0,0);
   scene.add(base);
 
+  const axesHelper = new THREE.AxesHelper( 5 );
+  axesHelper.position.set(0,1,0)
+  scene.add( axesHelper );
 
   //tabuleiro
   const texturatabuleiro = texturaLoader.load('./Imagens/base_tabuleiro.jpg');
@@ -57,6 +60,41 @@ function init() {
   peao2.name = "Amarelo";
   scene.add(peao2);
 
+
+  var texturaNeve = texturaLoader.load('./Imagens/textura_neve2.jpg')
+  //Primeiro boneco-neve
+  // Esfera de topo do boneco neve
+  var bonecoNeveTopoGeometria = new THREE.SphereGeometry(0.15)
+  var bonecoNeveTopoMaterial = new THREE.MeshBasicMaterial({map:texturaNeve}) 
+  var bonecoNeveTopo = new THREE.Mesh(bonecoNeveTopoGeometria,bonecoNeveTopoMaterial);
+  bonecoNeveTopo.position.set(0,1.04,0);
+  // Esfera do meio do boneco neve
+  var bonecoNeveMeioGeometria = new THREE.SphereGeometry(0.2)
+  var bonecoNeveMeioMaterial = new THREE.MeshBasicMaterial({map:texturaNeve}) 
+  var bonecoNeveMeio = new THREE.Mesh(bonecoNeveMeioGeometria,bonecoNeveMeioMaterial);
+  bonecoNeveMeio.position.set(0,0.8,0);
+  // Esfera de base do boneco neve
+  var bonecoNeveBaseGeometria = new THREE.SphereGeometry(0.3)
+  var bonecoNeveBaseMaterial = new THREE.MeshBasicMaterial({map:texturaNeve}) 
+  var bonecoNeveBase = new THREE.Mesh(bonecoNeveBaseGeometria,bonecoNeveBaseMaterial);
+  bonecoNeveBase.position.set(0,0.5,0);
+  // x = -5.5
+  // z = 3.5
+
+  
+  var bonecoNeveOlhoEsquerdoGeometria = new THREE.SphereGeometry(0.09);
+  
+  
+  const bonecoNeve1 = new THREE.Group();
+  bonecoNeve1.add(bonecoNeveBase,bonecoNeveMeio,bonecoNeveTopo);
+  bonecoNeve1.position.set(-5.5,0,4.5);
+  scene.add(bonecoNeve1);
+
+
+
+
+  
+
   //Cada quadrado no tabuleiro são 1x1 
 
   cameraP.position.set(0,15,5); 
@@ -81,7 +119,7 @@ function init() {
        if (numJogadas % 2 != 0) 
       {
         document.getElementById('numJogadas').innerText = "Vez do vermelho: ";
-        play(peao1,numDado, numJogadas);
+        play(bonecoNeve1,numDado, numJogadas);
       } 
       else
       {
@@ -139,64 +177,64 @@ function play(player, numDado, numJogadas)
   for(var i=0; i<numDado; i++)
   {
 
-    //Vitória 
-    if (player.position.x==-4.5 && player.position.z==-4.5)
-    {
-      alert("O "+ player.name +" ganhou!!");
-      location.reload();
-    }
-
+    
     //-----Mudanças de linha------
-
+    
     //Quando chega ao último quadrado da linha (sentido -> )
-   if(player.position.x==4.5)
-    {
-      player.position.z = player.position.z - 1;
-      numDado=numDado-1; //A subida também conta como um passo
-    }
-
-    //Quando chega ao último quadrado da linha (sentido <- )
-   if(player.position.x==-4.5 && (parseInt(player.position.z) % 2 != 0 || player.position.z<0 ))
-    {
-      player.position.z = player.position.z - 1;
+    if(player.position.x==4.5)
+      {
+        player.position.z = player.position.z - 1;
+        numDado=numDado-1; //A subida também conta como um passo
+      }
+      
+      //Quando chega ao último quadrado da linha (sentido <- )
+      if(player.position.x==-4.5 && (parseInt(player.position.z) % 2 != 0 || player.position.z<0 ))
+        {
+          player.position.z = player.position.z - 1;
       numDado=numDado-1; //A subida também conta como um passo
     }
     
-
+    
     //---Sentido do movimento do peão em cada linha----
-
+    
     //Linha cujo valor inteiro de Z é par e negativo
     if(parseInt(player.position.z) % 2 == 0 && player.position.z<0 ) 
-    {
-      player.position.x = player.position.x - 1;
-    }
-    //Linha cujo valor inteiro de Z é par e positivo
-    else if(parseInt(player.position.z) % 2 == 0 && player.position.z>0)
-    {
-      player.position.x = player.position.x + 1;
-    }
-
-    //Linha cujo valor inteiro de Z é impar e negativo
-   else if(parseInt(player.position.z) % 2 != 0 && player.position.z<0 )
-    {
-      player.position.x = player.position.x + 1;
-    }
-
-    //Linha cujo valor inteiro de Z é impar e positivo
-   else if(parseInt(player.position.z) % 2 != 0 && player.position.z>0)
-    {
-      player.position.x = player.position.x - 1;
-    }
-  }
-
-// Escadote
-if (player.position.x==-0.5 && player.position.z==4.5 )
-  {
-    player.position.set(-2.5,player.position.y,-0.5);
+      {
+        player.position.x = player.position.x - 1;
+      }
+      //Linha cujo valor inteiro de Z é par e positivo
+      else if(parseInt(player.position.z) % 2 == 0 && player.position.z>0)
+        {
+          player.position.x = player.position.x + 1;
+        }
+        
+        //Linha cujo valor inteiro de Z é impar e negativo
+        else if(parseInt(player.position.z) % 2 != 0 && player.position.z<0 )
+          {
+            player.position.x = player.position.x + 1;
+          }
+          
+          //Linha cujo valor inteiro de Z é impar e positivo
+          else if(parseInt(player.position.z) % 2 != 0 && player.position.z>0)
+            {
+              player.position.x = player.position.x - 1;
+            }
+          }
+          
+          // Escadote
+          if (player.position.x==-0.5 && player.position.z==4.5 )
+            {
+              player.position.set(-2.5,player.position.y,-0.5);
+          //Vitória 
+          if (player.position.x==-4.5 && player.position.z==-4.5)
+          {
+            alert("O "+ player.name +" ganhou!!");
+            location.reload();
+          }
   } 
 
   
-
+  
 }
 
 
